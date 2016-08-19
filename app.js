783e794827a82220dae0702b33e0940f465b847d
Elemento75 <elemento75@icloud.com>
2016-08-19 20:10:23 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var http   = require('http'),
+    static = require('node-static'),
+    app    = http.createServer(handler),
+    port   = process.env.PORT || 3000,
+    files  = new static.Server('./public', { cache: 7200 });
+
+function handler(request, response) {
+    request.addListener('end', function() {
+        files.serve(request, response);
+    }).resume();
+}
+
+app.listen(port);
+console.log('Your server goes on localhost:' + port);
